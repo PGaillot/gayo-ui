e561207c3dd8313b98d170c6dd46881865cdd0a9
PGaillot <pr.gaillot@gmail.com>
2024-08-06 22:10:24 +0200

    🪚 add folder drag & drop

--- projects/gayo-lib/src/lib/components/folder-card/folder-card.component.ts
+++ projects/gayo-lib/src/lib/components/folder-card/folder-card.component.ts
@@ -1,40 +1,82 @@
 import { NgStyle } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import { Component, ElementRef, Input, ViewChild } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 export interface FolderTab {
   title: string;
   content: string;
+  id: number;
 }
 
 @Component({
-  selector: 'lib-folder-card',
+  selector: 'gayo-folder-card',
   standalone: true,
   imports: [NgStyle],
   templateUrl: './folder-card.component.html',
   styleUrl: './folder-card.component.scss',
 })
+
+
 export class FolderCardComponent {
   @Input() tabs!: FolderTab[];
-  colors: string[] = ['#f6bd60', '#f7ede2', '#f5cac3', '#84a59d', '#f28482'];
-  tabsActiveIndex: number = 0;
-  tabsActive: FolderTab | undefined;
-  dragData?:string;
+  @Input() colors: string[] = [
+    '#ffe8cc',
+    '#ffd699',
+    '#ffc266',
+    '#84a59d',
+    '#ff9900',
+  ];
 
-  tabClick(index: number) {
-    this.tabsActiveIndex = index;
+
+  constructor(){
   }
 
-  onTabDragStart(event: any) {
-    this.dragData = event.target.innerText
-    console.log(event);
+  activeTabs!:BehaviorSubject<FolderTab>;
+
+  activeTabId: number | null = null;
+  draggedTabId: number | null = null;
+
+  onTabDragStart(event: DragEvent, tabId: number) {
+    this.draggedTabId = tabId;
   }
+
   onTabDragOver(event: DragEvent) {
     event.preventDefault();
   }
-  onTabDrop(event: DragEvent) {
-    event.preventDefault();
-    const targetTab:any = event.target;
-    targetTab.innerText = this.dragData;
 
+  onTabDrop(event: DragEvent, targetTabId: number) {
+    event.preventDefault();
+    if (this.draggedTabId === null || this.draggedTabId === targetTabId) return;
+
+    const draggedIndex = this.tabs.findIndex(
+      (tab) => tab.id === this.draggedTabId,
+    );
+    const targetIndex = this.tabs.findIndex((tab) => tab.id === targetTabId);
+
+    // Retirer l'onglet déplacé de sa position initiale
+    const draggedTab = this.tabs[draggedIndex];
+    this.tabs.splice(draggedIndex, 1);
+
+    // Insérer l'onglet à sa nouvelle position
+    this.tabs.splice(targetIndex, 0, draggedTab);
+
+    this.draggedTabId = null;
+  }
+
+  onTabClick(tabId: number) {
+    this.activeTabId = tabId;
+  }
+
+  getTabColor(tabId: number): string {
+    return this.colors[tabId];
+  }
+
+  getActiveTabContent(): string {
+    const activeTab = this.tabs.find((tab) => tab.id === this.activeTabId);
+    return activeTab ? activeTab.content : this.tabs[0].content;
+  }
+
+  ngOnInit(): void {
+    this.activeTabs = new BehaviorSubject<FolderTab>(this.tabs[0]);
   }
 }
--- projects/gayo-lib/src/stories/folder-card.stories.ts
+++ projects/gayo-lib/src/stories/folder-card.stories.ts
@@ -1,31 +1,45 @@
-import { Meta, StoryObj } from "@storybook/angular";
-import { FolderCardComponent } from "../public-api";
+import { Meta, StoryObj } from '@storybook/angular';
+import { FolderCardComponent } from '../public-api';
 
-const meta:Meta<FolderCardComponent> = {
-    title: 'FolderApp/FolderCard',
-    component: FolderCardComponent,
-    tags: ['autodocs'],
-}
+const meta: Meta<FolderCardComponent> = {
+  title: 'FolderApp/FolderCard',
+  component: FolderCardComponent,
+  tags: ['autodocs'],
+};
 
-export default meta
+export default meta;
 
 type Story = StoryObj<FolderCardComponent>;
 
 export const Default: Story = {
-    args: {
-        tabs: [
-            {
-                title: 'Tab 1',
-                content: 'laborum maiores mollitia dignissimos dolorum ullam atque. Velit totam explicabo at dignissimos eligendi amet reiciendis, quidem quasi repellendus?'
-            },
-            {
-                title: 'Tab 2',
-                content: 'molestiae corporis error quas quo sapiente vitae tenetur doloremque sunt suscipit fugit dicta. Perferendis cumque repellendus eaque totam tempore nesciunt nulla reiciendis. '
-            },
-            {
-                title: 'Tab 3',
-                content: 'Ipsum dolor sit amet consectetur adipisicing elit. Blanditiis laudantium amet, iusto ducimus qui ea at sequi quisquam nihil incidunt !'
-            }
-        ]
-    }
+  args: {
+    tabs: [
+      {
+        title: 'Histoires',
+        content:
+          'Histoires laborum maiores mollitia dignissimos dolorum ullam atque. Velit totam explicabo at dignissimos eligendi amet reiciendis, quidem quasi repellendus?',
+        id: 0,
+      },
+      {
+        title: 'Secrets',
+        content:
+          'Secrets molestiae corporis error quas quo sapiente vitae tenetur doloremque sunt suscipit fugit dicta. Perferendis cumque repellendus eaque totam tempore nesciunt nulla reiciendis.',
+        id: 1,
+      },
+      {
+        title: 'Paléontologie',
+        content:
+          'Paléontologie Ipsum dolor sit amet consectetur adipisicing elit. Blanditiis laudantium amet, iusto ducimus qui ea at sequi quisquam nihil incidunt !',
+        id: 2,
+      },
+    ],
+  },
+};
+
+
+export const colors: Story = {
+  args: {
+    ...Default.args,
+    colors: ['#ff595e', '#ffca3a', '#8ac926', '#1982c4', '#6a4c93']
+  } 
 }
