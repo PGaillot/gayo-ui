1edae4c3e79abe732287f4a8ba01728a55b4901c
PGaillot <pr.gaillot@gmail.com>
2024-08-10 19:44:27 +0200

    👍 publish-package.cjs & add slider component.

---
+++ projects/gayo-lib/src/stories/vault-tec/slider.stories.ts
@@ -0,0 +1,23 @@
+
+// Component slider was generated on Sat Aug 10 2024 19:43:33 GMT+0200 (heure d’été d’Europe centrale)
+
+import { Meta, StoryObj } from "@storybook/angular";
+import { SliderComponent } from "../../lib/components/vault-tec/slider/slider.component";
+
+
+const meta:Meta<SliderComponent> = {
+  title:'Vault-Tec/Slider',
+  component:SliderComponent,
+  tags: ['autodocs'],
+}
+
+export default meta;
+
+type Story = StoryObj<SliderComponent>;
+
+export const SimpleSlider:Story = {
+  args:{
+      // vos arguments ici
+  }
+}
+    
