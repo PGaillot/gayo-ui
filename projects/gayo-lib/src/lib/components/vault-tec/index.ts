e543cdfb224f0b07b40dbffc3ff7ba1c1d7fc2f3
PGaillot <pr.gaillot@gmail.com>
2024-08-10 10:41:01 +0200

    👍 add export from vault-tec components.

---
+++ projects/gayo-lib/src/lib/components/vault-tec/index.ts
@@ -0,0 +1,5 @@
+export * from './advice-block/advice-block.component'
+export * from './button/button.component'
+export * from './checkbox/checkbox.component'
+export * from './radio/radio.component'
+export * from './loader/loader.component'
--- projects/gayo-lib/src/public-api.ts
+++ projects/gayo-lib/src/public-api.ts
@@ -3,3 +3,4 @@
  */
 
 export * from './lib/components/folder-app/folder-card/folder-card.component';
+export * from './lib/components/vault-tec/'
