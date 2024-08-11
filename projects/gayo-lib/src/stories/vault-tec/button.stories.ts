766e58d54ba20a39ee1773b192047602d88f755e
PGaillot <pr.gaillot@gmail.com>
2024-08-11 21:47:40 +0200

    🖼️add stoke display ar button component.

--- projects/gayo-lib/src/stories/vault-tec/button.stories.ts
+++ projects/gayo-lib/src/stories/vault-tec/button.stories.ts
@@ -1,61 +1,89 @@
-import { Meta, StoryObj, argsToTemplate, moduleMetadata } from "@storybook/angular";
-import { ButtonComponent } from "../../lib/components/vault-tec/button/button.component";
+import {
+  Meta,
+  StoryObj,
+  argsToTemplate,
+  moduleMetadata,
+} from '@storybook/angular';
+import { ButtonComponent } from '../../lib/components/vault-tec/button/button.component';
 
 type BtnPropsAndCustomArgs = ButtonComponent & { label?: string };
 
-const meta:Meta<BtnPropsAndCustomArgs> = {
-    title:'Vault-Tec/Button',
-    component:ButtonComponent,
-    tags: ['autodocs'],
-    render: ({ label, ...args}) => ({
-        props:{
-            ...args,
-        },
-        template: `<vt-btn ${argsToTemplate(args)}>${label}</vt-btn>`,
-    })
-}
+const meta: Meta<BtnPropsAndCustomArgs> = {
+  title: 'Vault-Tec/Button',
+  component: ButtonComponent,
+  tags: ['autodocs'],
+  render: ({ label, ...args }) => ({
+    props: {
+      ...args,
+    },
+    template: `<vt-btn ${argsToTemplate(args)}>${label}</vt-btn>`,
+  }),
+};
 
 export default meta;
 
 type Story = StoryObj<BtnPropsAndCustomArgs>;
 
-export const SimpleButton:Story = {
-    args:{
-        label:'S.P.E.C.I.A.L',
-        size:'medium',
-    }
-}
+export const Demo: Story = {
+  args: {
+    size:'small'
+  },
+  render: (args) => ({
+    props: {
+      ...args,
+    },
+    template: `
+       <div style="display: flex; flex-direction: column; flex-wrap: wrap;  gap: 16px; font-family: Monospace; color: #2dc92d">
 
-export const SubcribeButton:Story = {
-    args:{
-        label:'Subscribe',
-    }
-}
+            <h2>Je veux en savoir plus</h2>
+            lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis laudantium amet, iusto ducimus qui ea at sequi quisquam nihil incidunt !
 
-export const Small:Story = {
-    args:{
-        label:'Subscribe',
-        size:'small',
-    }
-}
+            <div style="display: flex; flex-direction: row; flex-wrap: wrap;  gap: 16px; font-family: Monospace; color: #2dc92d">
+            <vt-btn ${argsToTemplate(args)}>Je veux en savoir plus</vt-btn>
+           <vt-btn ${argsToTemplate(args)} [stroke]="true">continuer</vt-btn>
+            </div>
+        </div>
+    `,
+  }),
+};
 
-export const Large:Story = {
-    args:{
-        label:'Subscribe',
-        size:'large',
-    }
-}
+export const SimpleButton: Story = {
+  args: {
+    label: 'S.P.E.C.I.A.L',
+    size: 'medium',
+  },
+};
 
-export const Disabled:Story = {
-    args:{
-        label:'Disabled',
-        disabled:true,
-    }
-}
+export const SubcribeButton: Story = {
+  args: {
+    label: 'Subscribe',
+  },
+};
 
-export const Loading:Story = {
-    args:{
-        label:'Loading',
-        loading:true,
-    }
-}
+export const Small: Story = {
+  args: {
+    label: 'Subscribe',
+    size: 'small',
+  },
+};
+
+export const Large: Story = {
+  args: {
+    label: 'Subscribe',
+    size: 'large',
+  },
+};
+
+export const Disabled: Story = {
+  args: {
+    label: 'Disabled',
+    disabled: true,
+  },
+};
+
+export const Loading: Story = {
+  args: {
+    label: 'Loading',
+    loading: true,
+  },
+};
