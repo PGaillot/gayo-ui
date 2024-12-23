3fd0d87cd3faf230f162443f19a0c3e775503134
PGaillot <pr.gaillot@gmail.com>
2024-12-23 17:19:09 +0100

    modified card

--- projects/gayo-lib/src/stories/summon-deamon/candle-card.stories.ts
+++ projects/gayo-lib/src/stories/summon-deamon/candle-card.stories.ts
@@ -15,7 +15,40 @@ type Story = StoryObj<CandleCardComponent>;
 
 export const BougieDeDebutant: Story = {
   args: {
-    cardName: 'Bougie de débutant',
-    diceNumber: [6,5,4]
+    cardName: 'bougie de débutant',
+    diceNumbers: [6,8],
+    id:71
+  }
+}
+
+export const CiergeMalefique: Story = {
+  args: {
+    cardName: 'cierge maléfique',
+    diceNumbers: [8,9],
+    id:72
+  }
+}
+
+export const DouceBougie: Story = {
+  args: {
+    cardName: 'douce bougie',
+    diceNumbers: [5,6],
+    id:70
+  }
+}
+
+export const GentilleBougie: Story = {
+  args: {
+    cardName: 'gentille bougie',
+    diceNumbers: [3,4,5],
+    id:69
+  }
+}
+
+export const CiergeDiabolique: Story = {
+  args: {
+    cardName: 'cierge diabolique',
+    diceNumbers: [9,10,11],
+    id:73
   }
 }
