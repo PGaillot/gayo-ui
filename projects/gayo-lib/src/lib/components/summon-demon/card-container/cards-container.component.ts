316d2b9a748fd2408285ceac745b1b07c8684fae
PGaillot <pr.gaillot@gmail.com>
2025-01-13 18:04:18 +0100

    feat: add cards container component for displaying candles, demons, and entities

---
+++ projects/gayo-lib/src/lib/components/summon-demon/card-container/cards-container.component.ts
@@ -0,0 +1,21 @@
+import { Component, Input } from "@angular/core";
+import { CandleCard, DemonCard, EntityCard } from "../../../models/lsd/card.model";
+import { NgClass, NgStyle } from "@angular/common";
+import { CandleCardComponent, DemonCardComponent, EntityCardComponent } from "gayo-lib";
+
+@Component({
+    selector: 'lsd-cards-container',
+    standalone: true,
+    templateUrl: './cards-container.component.html',
+    styleUrl: './cards-container.component.scss',
+    imports: [NgStyle, DemonCardComponent, EntityCardComponent, CandleCardComponent]
+})
+
+export class CardsContainerComponent {
+
+    @Input() candle: CandleCard | undefined;
+    @Input() demons: DemonCard[] = [];
+    @Input() entities: EntityCard[] = [];
+
+    @Input() orientation: 'ver' | 'hor' = 'hor';
+}
