cef8f16eb31a3b40aebdcfa3bca696c53146cbdb
PGaillot <pr.gaillot@gmail.com>
2025-02-19 18:22:50 +0100

    export backCard.

--- projects/gayo-lib/src/lib/components/summon-demon/index.ts
+++ projects/gayo-lib/src/lib/components/summon-demon/index.ts
@@ -1,3 +1,4 @@
 export * from './candle-card/candle-card.component'
 export * from './demon-card/demon-card.component'
 export * from './entity-card/entity-card.component'
+export * from './back-card/back-card.component'
